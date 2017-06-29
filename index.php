d931ada95aaa3ac13e2b3c6bb061bd44aabd8bcf
Ash Laidlaw <ashlaidlaw@gmail.com>
2017-06-29 10:25:14 -0700

    Small changes

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
     require_once('slack_url.php');
     $ch = curl_init();
-    $opts['token'] = $slack_token;
+    $opts = array(
+        'token' => $slack_token,
+        'presence' => 1
+    );
     curl_setopt($ch, CURLOPT_URL,'https://slack.com/api/users.list');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, true);
@@ -17,19 +20,21 @@
         <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
         <link href="css/styles.min.css" rel="stylesheet" type="text/css">
         <meta charset="utf-8">
-        <title>R/West Messenger</title>
+        <title>Welcome to R/West</title>
     </head>
     <body>
         <h2 id="welcome-title">Welcome to R/West</h2>
         <img src="img/rwest-logo-sm.png" alt="R/West Logo" id="main-logo">
         <?php if ($results->ok) : ?>
-        <form action="success.php" method="post" id="form">
+        <form action="success.php" method="post" name="form" id="form">
             <div id="emp-select">
                 <label for="employee">Who are you here to see?</label>
                 <select name="employee" id="emp-list">
                     <?php foreach($results->members as $member) : ?>
-                        <?php if ($member->profile->real_name_normalized && $member->deleted != 1 && $member->profile->real_name_normalized != 'slackbot' && $member->profile->real_name_normalized != 'Trello') : ?>
+                        <?php if ($member->profile->real_name_normalized && $member->deleted != 1 && $member->profile->real_name_normalized != 'slackbot' && $member->profile->real_name_normalized != 'Trello' && $member->presence === 'active') : ?>
                             <option value="<?php echo $member->id; ?>"><?php echo ucwords($member->profile->real_name_normalized); ?></option>
+                        <?php elseif ($member->presence === 'away' && $member->profile->real_name_normalized && $member->deleted != 1 && $member->profile->real_name_normalized != 'slackbot' && $member->profile->real_name_normalized != 'Trello') : ?>
+                            <option value="<?php echo $member->profile->email; ?>"><?php echo ucwords($member->profile->real_name_normalized); ?></option>
                         <?php endif; ?>
                     <?php endforeach; ?>
                 </select>
@@ -45,7 +50,12 @@
                 <input name="guest_org" type="text" required>
             </div>
 
-            <div type="submit" id="submit">Submit</div>
+            <div type="submit" id="submit" onclick="submitForm()">Submit</div>
+            <script>
+                function submitForm() {
+                    document.forms["form"].submit();
+                }
+            </script>
         </form>
         <?php else : ?>
             <h3>Sorry, we were not able to retrieve a list of employees. Please try again in a few minutes.</h3>
--- success.php
+++ success.php
@@ -1,23 +1,26 @@
 <?php
     require_once('slack_url.php');
-    $ch = curl_init();
     $guest_name = $_POST[filter_var('guest_name', FILTER_SANITIZE_MAGIC_QUOTES)];
     $guest_org = $_POST[filter_var('guest_org', FILTER_SANITIZE_MAGIC_QUOTES)];
-    $emp_id = $_POST['employee'];
     $message_to_send = 'Hey! ' . $guest_name . ' from ' . $guest_org . ' is here.';
-    $opts = array(
-        'token' => $slack_token,
-        'channel' => $emp_id,
-        'as_user' => 0,
-        'text' => $message_to_send,
-        'username' => 'R/West Guest'
-    );
-    curl_setopt($ch, CURLOPT_URL, 'https://slack.com/api/chat.postMessage');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $opts);
-    curl_exec($ch);
-    curl_close($ch);
+    if (strpos($_POST['employee'], '@') !== false) {
+        mail(strtolower($_POST['employee']) . ' is here!', $message_to_send);
+    } else {
+        $opts = array(
+            'token' => $slack_token,
+            'channel' => $_POST['employee'],
+            'as_user' => 0,
+            'text' => $message_to_send,
+            'username' => 'R/West Guest: ' . $guest_name
+        );
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://slack.com/api/chat.postMessage');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $opts);
+        curl_exec($ch);
+        curl_close($ch);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -26,15 +29,15 @@
         <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
         <link href="css/styles.min.css" rel="stylesheet" type="text/css">
         <meta charset="utf-8">
-        <title>Thanks!</title>
+        <title>Thanks! We'll be with you shortly.</title>
     </head>
     <body>
         <br><img src="img/rwest-logo-sm.png" alt="R/West Logo" id="main-logo">
-        <h3>Thanks <?php echo $guest_name ?>! Take a seat, and someone will be with you shortly.</h3>
-    </body>
+        <h3>Thanks <?php echo $guest_name ?>! Take a seat, and someone will be with you shortly. <?php echo $_POST['emp_email']?></h3>
+    </body>i
     <script>
         setTimeout(function() {
-            window.location = "http://localhost:8000/";
+            window.location = "http://rweststaging.com/RWest-Greeting-App/";
         }, 5000);
     </script>
 </html>
